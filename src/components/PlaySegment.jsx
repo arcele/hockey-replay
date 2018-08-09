1e1ba0c640422589a8a2525c3f423260b5e255aa
Ryan Kelley <ryan.kelley@espn.com>
2018-08-09 18:22:57 -0400

    jump to a play by clicking on it.

--- src/components/PlaySegment.jsx
+++ src/components/PlaySegment.jsx
@@ -9,19 +9,25 @@ class PlaySegment extends Component {
 		// yeah it did		
 	}
 
+	jumpTo(e, store) {
+		while(store.currentPlay < e.currentTarget.getAttribute('play') || store.currentSegment < e.currentTarget.getAttribute('segment')) {
+			store.next()
+		}
+	}
+
 	render() {
 		const styles = {fontSize:'40%', color:'#666'}
 		const { store, data, segmentId, playId } = this.props
 		let className = 'segment'
 		let key = `seg-${this.props.playId}-${this.props.segmentId}`
-		
+
 		if(store.currentPlay === playId && store.currentSegment === segmentId) {
 			// Highlight the current play segment
 			styles.backgroundColor = '#ddddee'
 			className = 'segment active-segment'
 		}
 		return(
-			<div className={className} key={key}>
+			<div className={className} key={key} play={this.props.playId} segment={this.props.segmentId} onClick={ (e) => this.jumpTo(e, this.props.store) }>
 				<div className="playId">{playId}</div>
 				<div className="segmentId">{segmentId}</div>
 				<div className="text">{data.text}</div>
